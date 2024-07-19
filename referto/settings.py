503a0a13ba3208a716b1ec0ff39d8e6b0d7bc7cf
eunjae <eunjae1004@snu.ac.kr>
2024-07-19 13:37:32 +0900

    Feat: 은재 장고 세팅 수정

--- referto/settings.py
+++ referto/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os, environ
 from datetime import timedelta
 
+SITE_ID = 1
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -34,6 +36,8 @@ MEDIA_URL = '/media/'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+print(f"OPENAI_API_KEY from settings: {OPENAI_API_KEY}")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
