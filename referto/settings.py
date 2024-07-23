e2a8cfd8673ba112a00a5721f11a7f0f1664b273
gs <rudtj0801@snu.ac.kr>
2024-07-23 20:37:40 +0900

    Feat: allowed_hosts 추가

--- referto/settings.py
+++ referto/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'referto-backend.fly.dev']
+ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'referto-backend.fly.dev','localhost']
 
 
 # Application definition
@@ -94,7 +94,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    # 'allauth.account.middleware.AccountMiddleware',
     
 ]
 
